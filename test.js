504494db1cc336f25d30cff1c01da5863ad3df46
오명섭 <oms1226@gmail.com>
2020-11-02 11:44:49 +0900

    javascript 주석 설명

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 // /Users/oms1226/_workspace/20201102_ncia_jsjq/day1_1.html 파일과 연결된 js 파일
 alert('Hello world in External');
 console.log('Hello world In External')
+// command + / 한줄 주석
+/*
+여러줄 주석
+
+*/
